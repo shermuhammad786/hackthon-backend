45d361a2c03f119fcac201d05e6b1507f4ae006a
shermuhammad786 <sherabr141@gmail.com>
2024-06-25 01:40:38 +0500

    limiter

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ app.use(express.json());
 
 connectDB();
 
-const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 15 minutes
-    limit: 4, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    // standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-    // legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-    // store: ... , // Use an external store for consistency across multiple server instances.
-    message: "Too many requests, please try again later.",
-})
+// const limiter = rateLimit({
+//     windowMs: 1 * 60 * 1000, // 15 minutes
+//     limit: 4, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+//     // standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+//     // legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+//     // store: ... , // Use an external store for consistency across multiple server instances.
+//     message: "Too many requests, please try again later.",
+// })
 
 // Apply the rate limiting middleware to all requests.
 app.use(limiter)
